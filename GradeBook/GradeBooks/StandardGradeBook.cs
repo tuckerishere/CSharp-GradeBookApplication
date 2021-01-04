83a0e62aa72dc7cf4d1ebab437ea7d5765162ddb
Michael Tucker <michael.tucker.a@gmail.com>
2021-01-04 17:50:01 -0600

    Added GradeBooks

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public GradeBookType Type { get; set; }
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;   
+        }
+    }
+}
